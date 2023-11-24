0b8b5a2248796e3b012ed1facd7ae1d4d16f8089
osalino <osazemen.odeh@gmail.com>
2023-11-24 01:41:11 +0000

    Navigation bar function with more categories - 05

---
+++ index.js
@@ -0,0 +1,22 @@
+window.onscroll = function() {
+    scrollFunction()
+}
+
+function scrollFunction() {
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+        document.getElementById("overlay").style.width = "10%";
+        document.getElementById("name").style.display = "none";
+        document.getElementById("name").style.transition = "2s";
+        document.getElementById("nav").style.marginLeft = "23px";
+        document.getElementById("profileimg").style.translate = "-55px";
+
+
+    } else {
+        document.getElementById("overlay").style.width = "20%";
+        document.getElementById("name").style.display = "block";
+        document.getElementById("nav").style.marginLeft = "100px";
+        document.getElementById("profileimg").style.marginRight = "2px";
+        document.getElementById("profileimg").style.translate = "30px";
+
+    }
+}
