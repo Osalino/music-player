dabac2869948df9b90eb7e8f6bd70360c078376b
osalino <osazemen.odeh@gmail.com>
2023-11-30 09:24:38 +0000

    Albums position been fixed, bg change, nav changed

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ window.onscroll = function() {
 
 function scrollFunction() {
     if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
-        document.getElementById("overlay").style.width = "10%";
+        document.getElementById("overlay").style.width = "12%";
         document.getElementById("name").style.display = "none";
         document.getElementById("name").style.transition = "2s";
-        document.getElementById("nav").style.marginLeft = "23px";
-        document.getElementById("profileimg").style.translate = "-55px";
+        document.getElementById("nav").style.marginLeft = "60px";
+        document.getElementById("profileimg").style.translate = "-25px";
         // document.getElementById("media-scrol").style.marginleft = "-10px";
 
 
     } else {
         document.getElementById("overlay").style.width = "20%";
         document.getElementById("name").style.display = "block";
-        document.getElementById("nav").style.marginLeft = "100px";
+        document.getElementById("nav").style.marginLeft = "110px";
         document.getElementById("profileimg").style.marginRight = "2px";
-        document.getElementById("profileimg").style.translate = "30px";
+        document.getElementById("profileimg").style.translate = "10px";
         // document.getElementById("media-scrol").style.marginLeft = "200px";
     }
 }
