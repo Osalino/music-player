49847ff2fbdbf8053e27cb30d72c14d565a3bc59
rmadd06 <rmadds806@gmail.com>
2023-11-30 17:59:05 +0000

    New progress bar

--- index.js
+++ index.js
@@ -21,3 +21,39 @@ function scrollFunction() {
         // document.getElementById("media-scrol").style.marginLeft = "200px";
     }
 }
+
+/* const progressElem = document.querySelector('#progress')
+const html = document.querySelector('html')
+
+function scrollHandler() {
+    const screenHeight = window.innerHeight
+    const scrollY = Math.round(window.scrollY)
+    const maxScreenHeight = html.scrollHeight
+    
+    const howMuchScroll = screenHeight + scrollY
+    const progress = (howMuchScroll / maxScreenHeight) * 100
+    
+    progressElem.value = progress
+}
+
+scrollHandler()
+
+window.addEventListener('scroll', scrollHandler) */
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const progressElem = document.querySelector('#progress')
+ 
+    function scrollHandler() {
+        const scrollY = Math.round(window.scrollY)
+        const maxScrollHeight = document.body.scrollHeight - window.innerHeight
+ 
+        const progress = (scrollY / maxScrollHeight) * 100
+ 
+        progressElem.value = progress
+    }
+ 
+    scrollHandler()
+ 
+    window.addEventListener('scroll', scrollHandler)
+ });
+ 
