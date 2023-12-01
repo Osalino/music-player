77fd21f2228a8240a32959b95f4144a1e95065ba
rmadd06 <rmadds806@gmail.com>
2023-12-01 19:37:00 +0000

    Started Deezer API integration

---
+++ api.js
@@ -0,0 +1,10 @@
+var d = new DZ.init({
+    appId : "651291",
+    channelUrl : "https://example.com/callback/index.html",
+    player : {
+        onload : function() {
+            
+        }
+    }
+ });
+ 
